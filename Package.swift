51c3d8fae93a6f709878a2dd82fa822cc298fbad
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-29 08:15:18 -0500

    refactor dep urls for merge

--- Package.swift
+++ Package.swift
@@ -10,13 +10,8 @@ let package = Package(
         .library(name: "UserModule", targets: ["UserModule"]),
     ],
     dependencies: [
-//		.package(path: "../feather-core"),
-//		.package(path: "../user-objects"),
-        .package(url: "https://github.com/Rando-Coderissian/feather-core", .branch("test-refactored-modules")),
-        .package(url: "https://github.com/Rando-Coderissian/user-objects", .branch("test-refactor-modules")),
-		
-//        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
-//        .package(url: "https://github.com/feathercms/user-objects", .branch("main")),
+       .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
+       .package(url: "https://github.com/feathercms/user-objects", .branch("main")),
     ],
     targets: [
         .target(name: "UserModule",
